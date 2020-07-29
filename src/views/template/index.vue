6d7950b0af9fca37d8279d14ab223d54a0af27ea
wuqiong <wuqiong@wq-mp.local>
2020-07-29 18:48:35 +0800

    init

---
+++ src/views/template/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>模版</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  },
+
+  beforeCreate () {},
+
+  created () {},
+
+  beforeMount () {},
+
+  mounted () {},
+
+  beforeUpdate () {},
+
+  updated () {},
+
+  beforeDestroy () {},
+
+  destroyed () {},
+
+  computed: {},
+
+  methods: {},
+
+  watch: {},
+
+  components: {}
+}
+</script>
+
+<style scoped lang="">
+</style>
