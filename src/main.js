db0757bc9246a65fa1624ff7540cacb450af879e
wuqiong <wuqiong@wq-mp.local>
2020-07-30 12:10:18 +0800

    删除ant-desgin

--- src/main.js
+++ src/main.js
@@ -1,13 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-import Antd from 'ant-design-vue'
 import router from './router'
 import store from './store'
-import 'ant-design-vue/dist/antd.css'
 
 Vue.config.productionTip = false
-
-Vue.use(Antd)
 
 new Vue({
   router,
--- src/views/template/index.vue
+++ src/views/template/index.vue
@@ -1,36 +1,13 @@
 <template>
   <div class="template">
-    <div class="template-header">
-      <p>全部项目</p>
-      <a-button type="primary" icon="plus" @click="showModal = true">新建项目</a-button>
-    </div>
-    <a-divider />
-    <a-modal
-      v-model="showModal"
-      title="选择模版"
-      @ok="handleSelectedTemp"
-    >
-      <a-radio-group button-style="solid" v-model="selectedTemp">
-        <a-radio-button v-for="(item, index) in templates" :key="index" :value="item.type">
-          {{ item.name }}
-        </a-radio-button>
-      </a-radio-group>
-    </a-modal>
+    模版
   </div>
 </template>
 
 <script>
 export default {
   data () {
-    return {
-      showModal: false,
-      templates: [
-        { type: 'web', name: '电脑' },
-        { type: 'h5', name: '手机' },
-        { type: 'minipro', name: '小程序' }
-      ],
-      selectedTemp: ''
-    }
+    return {}
   },
 
   beforeCreate () {},
@@ -51,23 +28,7 @@ export default {
 
   computed: {},
 
-  methods: {
-    handleSelectedTemp () {
-      if (!this.selectedTemp) {
-        this.$message.warning({
-          content: '请选择模版'
-        })
-        return
-      }
-      this.showModal = false
-      this.$router.push({
-        path: '/editor',
-        query: {
-          temp: this.selectedTemp
-        }
-      })
-    }
-  },
+  methods: {},
 
   watch: {},
 
